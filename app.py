c2033087b65171d6b025446121f2bac8fcf4f558
pakkopacoyau-lgtm <pakkopacoyau@gmail.com>
2026-04-20 17:41:14 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
 import streamlit as st
 
-st.title("Welcome to Streamlit")
+st.title("Welcome to Streamlit!")
+
+st.write("Hello, Streamlit!")
+st.write(12345)
+st.write({"Name": "Alice", "Age": 30})
+
+st.write(['list1'.'list2'])
